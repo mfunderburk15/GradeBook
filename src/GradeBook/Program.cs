69aadc9b3c85f24e95aa6b332518623f65e7c726
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-12-01 14:42:16 -0600

    you can now pass in an argument through the command line

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,7 +6,7 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Masen!");
+            Console.WriteLine($"Hello {args[0]}!");
         }
     }
 }
